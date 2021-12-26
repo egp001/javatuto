7f728147af1cf3b2839345b1692f15b1ecd3ee70
egarcia <egarcia@egarcia-PC>
2021-12-26 14:54:22 -0600

    my first class java

---
+++ time-proyect/src/main/java/com/cat/time/MainTime.java
@@ -0,0 +1,23 @@
+package com.cat.time;
+
+/**
+ * @author egarcia
+ *
+ */
+public class MainTime {
+	
+	public static void main(String[] args) {
+		System.out.println("vamos a sumar:");
+		Integer valor1 = Integer.valueOf(10); 
+		Integer valor2 = Integer.valueOf(20);
+		Integer valor3 = Integer.valueOf(30);
+		Integer resultado = valor1 + valor2 + valor3;
+		
+		
+		System.out.println ("Resultado de la suma:" + resultado);
+		
+	}
+
+}
+
+
